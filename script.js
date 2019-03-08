3b0f4fbd6cf5563b5fbcfda3307d8041f7b4a97e
Sofronov89 <sofronov89@mail.ru>
2019-03-08 23:41:46 +0300

    work_jino_page

---
+++ script.js
@@ -0,0 +1,5 @@
+function addText() {
+    let val = document.getElementById('inp').value;
+    document.getElementById('resText').innerHTML= "Домен "+val+" - свободен. ";
+    document.getElementById('registr').innerHTML= "Зарегистрировать за 39₽";
+};
